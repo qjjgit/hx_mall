9d52166cc017256337c8a35a52bfb8e79e49fb6f
QQQ <969182578@qq.com>
2021-05-09 18:54:59 +0800

    5.9

---
+++ mall-pojo/src/main/java/com/hx/mall/form/ShopRegisterForm.java
@@ -0,0 +1,16 @@
+package com.hx.mall.form;
+
+import lombok.Data;
+
+@Data
+public class ShopRegisterForm {
+
+    private String phone;
+
+    private String email;
+
+    private String code;
+
+    private String password;
+
+}
